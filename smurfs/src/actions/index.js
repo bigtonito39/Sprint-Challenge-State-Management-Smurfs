cf22bc5647291102d7d5ce9cce27f05bbf98da94
bigtonito39 <bigtonito@hotmail.com>
2020-01-13 18:39:43 -0500

    Form to send data to POST made, data sent from reducer to smurfcomponent, which then gets mapped to render data,MVP met

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -7,18 +7,34 @@ export const getSmurfs = () => {
        .get("http://localhost:3333/smurfs")
        .then( res => {
             
-                      //Just do map to start showign names...
-           dispatch({type: "NEW_SMURF", payload: res.data/*res.data.map( data => {
+                      
+           dispatch({type: "NEW_SMURF", payload:
+           res.data.map( data => {
                return {name:data.name,
                age:data.age,
                height:data.height,
                id:data.id,
                loading: false }
-           })*/})
+           })})
        })
        .catch(err => console.log("ERROR", err))
    }
 
+}
+
+export const addSmurf = (incominData) => {
+    return dispatch => {
+       axios
+       .post("http://localhost:3333/smurfs",incominData)
+       .then(res => {
+           console.log(res)
+       })
+       .catch( error => {
+           console.log(error)
+       })
+
+        dispatch({type:"ADD_SMURF"})
+    }
 }
 
 
--- smurfs/src/components/smurf.js
+++ smurfs/src/components/smurf.js
@@ -1,27 +1,33 @@
 import React from "react";
 import  {connect} from "react-redux";
 import { getSmurfs } from "../actions/index";
+import SmurfForm from "./smurfForm"
+
 
 function smurf (props) {
-console.log(props.name)
+console.log(props.smurfs)
     return ( 
     <div>
         {props.loading ? (
         <h1>Loading...</h1>
       ) : 
-      <h1>{`Name: ${props.name} / Age: ${props.age} / height: ${props.height}`}</h1>
+      props.smurfs.map(data => {
+        return <h1>{`Name: ${data.name} / Age: ${data.age} / height: ${data.height}`}</h1>
+      })
         
       }
      <button onClick={ (e)=> {
          e.preventDefault()
          props.getSmurfs()
      }}> Show First API Smurf</button>
-
-      
+<div>
+   
+</div>
+      <SmurfForm />
     </div>)
 }
 
-const mapStateToProps = state => {
-    return {name: state.name, age: state.age, height:state.height, id: state.id, loading:state.loading}
-}
+const mapStateToProps = state => ({
+    smurfs:state.smurfs
+})
 export default connect(mapStateToProps, {getSmurfs}) (smurf);
